f245c99b5cdfe7f4ebe152ee4fcb4a4072d616ac
Luke Giuliani <luke@giuliani.com.au>
2018-06-08 17:51:37 +1000

    successful build with new ns records

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/adamdecaf/terraform-provider-namecheap/namecheap"
+	//	"github.com/adamdecaf/terraform-provider-namecheap/namecheap"
 	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-namecheap/namecheap"
 )
 
 func main() {
