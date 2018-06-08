1db6dcf7ced267f6539513cc64ec288a73fd5a72
Adam Shannon <adamkshannon@gmail.com>
2018-06-08 09:46:54 -0500

    fix imports / compile

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	//	"github.com/adamdecaf/terraform-provider-namecheap/namecheap"
+	"github.com/adamdecaf/terraform-provider-namecheap/namecheap"
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-namecheap/namecheap"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: namecheap.Provider})
+		ProviderFunc: namecheap.Provider,
+	})
 }
