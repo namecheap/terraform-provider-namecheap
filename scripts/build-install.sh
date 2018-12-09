33faa5b40c1e3af34894f054756180662ed26bbe
Joshua Copeland <joshuarcopeland@gmail.com>
2018-12-09 13:39:28 -0800

    gopath fix

--- scripts/build-install.sh
+++ scripts/build-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 make build
-mv /Users/ogp/go-workspace/bin/terraform-provider-namecheap ~/.terraform.d/plugins/terraform-provider-namecheap
+mv $GOPATH/bin/terraform-provider-namecheap ~/.terraform.d/plugins/terraform-provider-namecheap
 chmod +x ~/.terraform.d/plugins/terraform-provider-namecheap
